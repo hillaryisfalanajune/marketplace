6b938c8a4a9c6a1debd7edcc4aebea4237bf1061
hillaryisfalanajune <hillaryisfalana09@gmail.com>
2024-05-21 11:03:49 +0700

    Api Routes

--- Barbeq/routes/api.php
+++ Barbeq/routes/api.php
@@ -1,7 +1,23 @@
+
+
 <?php
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ApiController;
+use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\ProdukController;
+use App\Http\Controllers\Api\WishlistController;
+use App\Http\Controllers\Api\CartController; // Perbaikan 2: Mengimpor CartController
+use App\Http\Controllers\Api\PromoController;
+use App\Http\Controllers\Api\UserProfileController;
+use App\Http\Controllers\Api\TransaksiController;
+use App\Http\Controllers\Api\Detailcontroller;
+use App\Http\Controllers\Api\PesananController;
+use App\Http\Controllers\Api\BannerController;
+
+
+use App\Models\Pembeli;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +30,64 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->get('/Pembeli', function (Request $request) {
+    return $request->pembeli();
 });
+
+Route::post("register", [ApiController::class, "register"]);
+Route::post("login", [ApiController::class, "login"]);
+
+Route::group([
+    "middleware" => ["auth:api"]
+], function(){
+    // Route::get("profile", [ApiController::class, "fetch"]);
+    Route::get("refresh", [ApiController::class, "refreshToken"]);
+    Route::get("logout", [ApiController::class, "logout"]);
+});
+
+Route::get("/get", [ProdukController::class, "fetch"]);
+Route::get("profile", [UserProfileController::class, "profile"]);
+Route::post('/produk', [ProdukController::class, "store"]);
+Route::post('produk/search/', [ProdukController::class, "search"]);
+Route::put('produk/{id}', [ProdukController::class, "update"]);
+Route::get('produk', [ProdukController::class, "index"]);
+Route::get('produkkat/', [ProdukController::class, "getByCategory"]);
+Route::get('produk/{id}', [ProdukController::class, "show"]);
+
+Route::get('wishlist', [WishlistController::class, "index"]);
+Route::get('wishlist/{id}', [WishlistController::class, "show"]);
+Route::get('banner', [BannerController::class, "index"]);
+Route::get('wishlist/{id}', [WishlistController::class, "show"]);
+
+Route::post('wishlist', [WishlistController::class, "store"]);
+Route::delete('wishlist/{id}', [WishlistController::class, "destroy"]);
+
+Route::get('kategori', [ProdukController::class, "kategori"]);
+Route::post('pesanan', [PesananController::class, "store"]);
+Route::get('pesanan', [PesananController::class, "index"]);
+
+Route::get("/get", [ProdukController::class, "fetch"]);
+
+Route::get("/get", [CartController::class, "fetch"]);
+Route::post('cart', [CartController::class, "store"]); // Perbaikan 4: Menggunakan CartController
+Route::delete('cart/{id}', [CartController::class, "destroy"]); // Perbaikan 4: Menggunakan CartController
+Route::put('cart/{id}', [CartController::class, "update"]); // Jika ada endpoint update
+Route::get('cart', [CartController::class, "index"]);
+
+// Route::get('user', [UserProfileController::class, "index"]);
+Route::post('user', [UserProfileController::class, "update"]);
+
+
+
+Route::get("/get", [TransaksiController::class, "fetch"]);
+Route::post('transaksi', [TransaksiController::class, "store"]);
+Route::put('transaksi/{id}', [TransaksiController::class, "update"]);
+Route::get('transaksi',[TransaksiController::class, "index"]);
+
+
+
+
+Route::get("/get", [PromoController::class, "fetch"]);
+Route::post('promo', [PromoController::class, "store"]);
+Route::put('promo/{id}', [PromoController::class, "update"]);
+Route::get('promo',[PromoController::class, "index"]);
--- Barbeq/routes/web.php
+++ Barbeq/routes/web.php
@@ -1,6 +1,28 @@
 <?php
 
+use App\Models\User;
+use App\Models\Produk;
+use App\Models\Kategori;
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\ProfilController;
+use App\Http\Controllers\PembeliController;
+use App\Http\Controllers\PenjualController;
+use App\Http\Controllers\PesananController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\KategoriController;
+// use App\Http\Controllers\ProfilController;
+use App\Http\Controllers\KeuanganController;
+use App\Http\Controllers\PasswordController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\RekeningController;
+use App\Http\Controllers\PengirimanController;
+use App\Http\Controllers\EditPasswordController;
+use App\Http\Controllers\UserController; // tambahkan ini untuk UserController
+
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +30,97 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home',['title'=>'Home Pages', 'active' => 'home']);
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index',['title' => 'Home']);
+})->middleware('auth'); // untuk proteksi
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest'); // untuk menampilkan form
+Route::post('/login', [LoginController::class, 'authenticate']); // memproses form
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
+
+Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/register', [RegisterController::class, 'store']);
+
+// Route::resource('/produk', ProdukController::class)->except('show')->middleware('auth');
+
+Route::resource('/pembeli', PembeliController::class)->except('show')->middleware('auth','admin')->except('show');
+Route::resource('/pengiriman', PengirimanController::class)->except('show')->middleware('auth');
+
+
+Route::resource('/pesanan', PesananController::class)->except('show')->middleware('auth');
+
+
+Route::resource('/keuangan', KeuanganController::class)->except('show')->middleware('auth');
+Route::resource('/kategori', KategoriController::class)->except('show');
+
+
+Route::group(['middleware' => ['auth', 'superadmin']], function () {
+    Route::get('/user', [UserController::class, 'manageUsers'])->name('user.manage');
+});
+
+Route::put('/pengiriman/{id}/update-status', [PengirimanController::class, 'updateStatus'])->name('pengiriman.updateStatus');
+Route::put('/pesanan/{id}/update-statusverifikasi', [PesananController::class, 'updateStatusverifikasi'])->name('pesanan.updateStatusverifikasi');
+// Route::put('/pesanan/{id}/update-statusverifikasi', [PesananController::class, 'updateStatusverifikasi'])->name('pesanan.updateStatusverifikasi');
+
+
+Route::middleware(['auth', 'superadmin'])->group(function () {
+    Route::get('/user', [UserController::class, 'index'])->name('user.manage');
+       Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user.destroy'); // Perbaikan di sini
+    Route::post('/user', [UserController::class, 'store'])->name('user.store'); // Perbaikan di sini
+    Route::get('/user/create', [UserController::class, 'create'])->name('user.create');
+});
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/profil', [ProfilController::class, 'index'])->name('profil.index');
+    Route::get('/profil/create', [ProfilController::class, 'create'])->name('profil.create');
+    Route::post('/profil', [ProfilController::class, 'store'])->name('profil.store');
+    Route::get('/profil/edit/{id}', [ProfilController::class, 'edit'])->name('profil.edit'); // Tambahkan definisi route edit
+    Route::put('/profil/update/{id}', [ProfilController::class, 'update'])->name('profil.update');
+
+});
+
+// Rute untuk admin
+Route::middleware(['auth'])->group(function () {
+    Route::resource('/penjual', PenjualController::class)->except('show');
+});
+Route::middleware(['auth'])->group(function () {
+    Route::resource('/produk', ProdukController::class)->except('show');
+});
+
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/setting', [SettingController::class, 'index'])->name('setting.index');
+});
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/password/edit', [PasswordController::class, 'edit'])->name('password.edit');
+    Route::post('/password/update', [PasswordController::class, 'update'])->name('password.update');
+});
+
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/rekening', [RekeningController::class, 'index'])->name('rekening.index');
+    Route::get('/rekening/create', [RekeningController::class, 'create'])->name('rekening.create');
+    Route::post('/rekening/store', [RekeningController::class, 'store'])->name('rekening.store');
+    Route::get('/rekening/{rekening}/edit', [RekeningController::class, 'edit'])->name('rekening.edit'); // Definisi route untuk halaman edit
+    Route::put('/rekening/{rekening}', [RekeningController::class, 'update'])->name('rekening.update');
+    Route::delete('/rekening/{rekening}', [RekeningController::class, 'destroy'])->name('rekening.destroy');
+});
+
+
+
+
+?>
